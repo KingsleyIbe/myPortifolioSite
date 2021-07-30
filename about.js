e8d75846d1be564beeabc406991122b54b3e74d4
KingsleyIbe <ibekingsley66@gmail.com>
2021-07-30 16:11:20 +0100

    new update

--- about.js
+++ about.js
@@ -87,23 +87,23 @@ function bootstrapFunction() {
         bootstrapDisplay.style.display = "block";
     }
 }
-var modal = document.getElementById('myModal');
+// var modal = document.getElementById('myModal');
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = $('.myImg');
-var modalImg = $("#img01");
-var captionText = document.getElementById("caption");
-$('.myImg').click(function() {
-    modal.style.display = "block";
-    var newSrc = this.src;
-    modalImg.attr('src', newSrc);
-    captionText.innerHTML = this.alt;
-});
+// var img = $('.myImg');
+// var modalImg = $("#img01");
+// var captionText = document.getElementById("caption");
+// $('.myImg').click(function() {
+//     modal.style.display = "block";
+//     var newSrc = this.src;
+//     modalImg.attr('src', newSrc);
+//     captionText.innerHTML = this.alt;
+// });
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
+// span.onclick = function() {
+//     modal.style.display = "none";
+// }
