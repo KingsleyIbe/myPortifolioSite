68e66d14c94a76ba47b1f9fdc17ac43c5ecbd079
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2022-06-25 08:07:20 +0100

    Add intro component

--- src/component/Header/Home.jsx
+++ src/component/Header/Home.jsx
@@ -1,112 +1,11 @@
 import React from 'react';
 import Header from './Header';
+import Intro from './Intro';
 
 const Home = () => (
   <main>
-    <div className="mb-[1000px]">
-      <Header />
-    </div>
-    <section id="portfolio">
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Cupiditate, molestiae commodi excepturi debitis corporis maxime nulla vero adipisci quia.
-        Odio perferendis eligendi consequatur nemo exercitationem labore fugiat error
-        veniet maxime.
-      </p>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-        Corporis sit quae minus, at sapiente veniam, totam natus
-        et deleniti voluptates eum eos. Iusto,
-        enim! Alias mollitia illum autem eos? Aliquid.
-      </p>
-    </section>
-    <section id="">
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Cupiditate, molestiae commodi excepturi debitis corporis maxime nulla vero adipisci quia.
-        Odio perferendis eligendi consequatur nemo exercitationem
-        labore fugiat error eveniet maxime.
-      </p>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-        Corporis sit quae minus, at sapiente veniam, totam natus et
-        deleniti voluptates eum eos. Iusto,
-        enim! Alias mollitia illum autem eos? Aliquid.
-      </p>
-    </section>
-    <section id="about">
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate,
-        molestiae commodi excepturi debitis corporis maxime nulla vero adipisci quia.
-        Odio perferendis eligendi consequatur nemo exercitationem
-        labore fugiat error eveniet maxime.
-      </p>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-        Corporis sit quae minus, at sapiente veniam, totam natus
-        et deleniti voluptates eum eos. Iusto,
-        enim! Alias mollitia illum autem eos? Aliquid.
-      </p>
-    </section>
-    <section id="">
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Cupiditate, molestiae commodi excepturi debitis corporis maxime nulla vero adipisci quia.
-        Odio perferendis eligendi consequatur nemo exercitationem
-        labore fugiat error eveniet maxime.
-      </p>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-        Corporis sit quae minus, at sapiente veniam, totam natus
-        et deleniti voluptates eum eos. Iusto,
-        enim! Alias mollitia illum autem eos? Aliquid.
-
-      </p>
-    </section>
-    <section id="contact">
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Cupiditate, molestiae commodi excepturi debitis corporis maxime nulla vero adipisci quia.
-        Odio perferendis eligendi consequatur nemo exercitationem
-        labore fugiat error eveniet maxime.
-      </p>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-        Corporis sit quae minus, at sapiente veniam, totam natus
-        et deleniti voluptates eum eos. Iusto,
-        enim! Alias mollitia illum autem eos? Aliquid.
-      </p>
-    </section>
-    <section id="">
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Cupiditate, molestiae commodi excepturi debitis corporis maxime nulla vero adipisci quia.
-        Odio perferendis eligendi consequatur nemo exercitationem
-        labore fugiat error eveniet maxime.
-      </p>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis
-        sit quae minus, at sapiente veniam, totam natus et deleniti voluptates eum eos. Iusto,
-        enim! Alias mollitia illum autem eos? Aliquid.
-      </p>
-    </section>
-    <section id="envelop">
-      <p>
-        Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Cupiditate, molestiae commodi
-        excepturi debitis corporis maxime nulla vero adipisci quia.
-        Odio perferendis eligendi consequatur nemo exercitationem
-        abore fugiat error eveniet maxime.
-
-      </p>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-        Corporis sit quae minus, at sapiente veniam, totam natus et
-        deleniti voluptates eum eos. Iusto,
-        enim! Alias mollitia illum autem eos? Aliquid.
-
-      </p>
-    </section>
+    <Header />
+    <Intro />
   </main>
 );
 
