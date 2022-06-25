b938ea5785c24d5a0477eb895af303d7f95612a9
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2022-06-25 05:55:49 +0100

    Update header component

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -4,17 +4,17 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => (
-  <div className="header bg-[#3c3a39] p-10 fixed z-50">
-    <header>
-      <nav className="flex gap-x-10 items-center text-[20px] font-bold text-[#fff]">
+  <div className="header bg-[#3c3a39] p-10">
+    <header className="">
+      <nav className="flex gap-x-10 items-center text-[20px] font-bold text-[#fff] relative">
         <span>
           <h1>Let&#39;s connect</h1>
         </span>
-        <ul className="flex gap-x-10 items-center justify-end">
-          <li><NavLink to="/">Portfolio</NavLink></li>
-          <li><NavLink to="/#about">About</NavLink></li>
-          <li><NavLink to="/#contact">Contact</NavLink></li>
-          <li><NavLink to="/#envelop"><FontAwesomeIcon icon={faEnvelope} /></NavLink></li>
+        <ul className="flex gap-x-10 items-center absolute right-0">
+          <li><NavLink to="/" className="hover:opacity-[0.5]">Portfolio</NavLink></li>
+          <li><NavLink to="/#about" className="hover:opacity-[0.5]">About</NavLink></li>
+          <li><NavLink to="/#contact" className="hover:opacity-[0.5]">Contact</NavLink></li>
+          <li><NavLink to="/#envelop"><FontAwesomeIcon icon={faEnvelope} className="text-[30px]" /></NavLink></li>
         </ul>
       </nav>
     </header>
