0207c93df8f4fc2953e71a747a3a567072a78624
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2022-06-25 05:32:02 +0100

    Update header component

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => (
-  <div>
+  <div className="header bg-[#3c3a39] p-10 fixed z-50">
     <header>
-      <nav>
-        <ul>
+      <nav className="flex gap-x-10 items-center text-[20px] font-bold text-[#fff]">
+        <span>
+          <h1>Let&#39;s connect</h1>
+        </span>
+        <ul className="flex gap-x-10 items-center justify-end">
           <li><NavLink to="/">Portfolio</NavLink></li>
           <li><NavLink to="/#about">About</NavLink></li>
           <li><NavLink to="/#contact">Contact</NavLink></li>
-          <li><NavLink to="/#envelop">Envelop</NavLink></li>
+          <li><NavLink to="/#envelop"><FontAwesomeIcon icon={faEnvelope} /></NavLink></li>
         </ul>
       </nav>
     </header>
