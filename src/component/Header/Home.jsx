d8edc76fe2976705ee9fd0f2c1cfb2c69e753afc
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2022-06-25 10:05:52 +0100

    Add recent project component

--- src/component/Header/Home.jsx
+++ src/component/Header/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './Header';
 import Intro from './Intro';
+import RecentWorks from './RecentWorks';
 
 const Home = () => (
   <main>
     <Header />
     <Intro />
+    <RecentWorks />
   </main>
 );
 
