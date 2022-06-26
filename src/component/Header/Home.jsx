6fab56af2527333ee7fbe91f5667512d99b34a3b
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2022-06-26 22:51:26 +0100

    Add getintouch component

--- src/component/Header/Home.jsx
+++ src/component/Header/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import AboutMe from './AboutMe';
+import Footer from './Footer';
 import GetInTouch from './GetInTouch';
 import Header from './Header';
 import Intro from './Intro';
@@ -12,6 +13,7 @@ const Home = () => (
     <RecentWorks />
     <AboutMe />
     <GetInTouch />
+    <Footer />
   </main>
 );
 
